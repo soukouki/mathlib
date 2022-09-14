d9868e12ccc6efa1a306017f75654649382fd3e6
soukouki <soukouki0@yahoo.co.jp>
2022-09-14 14:10:10 +0900

    2.5まで

--- ensembles.v
+++ ensembles.v
@@ -9,7 +9,6 @@ Variable U: Type.
 Definition Ensemble := U -> Prop.
 
 Definition In (a: U) (A: Ensemble) := A a.
-
 Notation "a \in A" := (In a A) (at level 11).
 
 Definition EmptySet: Ensemble := fun _ => False.
@@ -19,7 +18,6 @@ Axiom ensemble_eq: forall (A B: Ensemble),
   (forall (x: U), (x \in A <-> x \in B)) -> A = B.
 
 Definition Subset (A B: Ensemble) := forall x, x \in A -> x \in B.
-
 Notation "A \subset B" := (Subset A B) (at level 11).
 
 (* (1.3) *)
@@ -29,25 +27,22 @@ move=> A B.
 split.
 - move=> HA_eq_B.
   split.
-  + rewrite /Subset.
-    move=> x Hx_in_A.
-    by rewrite -HA_eq_B.
-  + rewrite /Subset.
-    move=> x Hx_in_B.
-    by rewrite HA_eq_B.
+  + rewrite HA_eq_B.
+    by rewrite /Subset.
+  + rewrite HA_eq_B.
+    by rewrite /Subset.
 - case.
   move=> HA_subset_B HB_subset_A.
   apply ensemble_eq.
   move=> x.
   split.
-  + move=> Hx_in_A.
-    by apply HA_subset_B.
-  + move=> Hx_in_B.
-    by apply HB_subset_A.
+  + by apply HA_subset_B.
+  + by apply HB_subset_A.
 Qed.
 
 (* (1.4)
-   本にあるのは A \subset B /\ B \subset C だけれど、明らかに同等な上にこちらのほうがCoq的に扱いやすいのでこう書いた *)
+   本にあるのは A \subset B /\ B \subset C だけれど、明らかに同等な上にこちらのほうがCoq的に扱いやすいのでこう書いた
+   今後も同じような例が出てくるが、同様に行う *)
 Theorem subset_trans: forall (A B C: Ensemble), A \subset B -> B \subset C -> A \subset C.
 Proof.
 move=> A B C HA_subset_B HB_subset_C.
@@ -65,5 +60,80 @@ move=> x.
 rewrite /In.
 by rewrite /EmptySet.
 Qed.
+
+Definition Cup (A B: Ensemble) := fun x: U => x \in A \/ x \in B.
+Notation "A \cup B" := (Cup A B) (at level 10).
+
+(* (2.2.1) *)
+Theorem subset_cup_l: forall (A B: Ensemble), A \subset A \cup B.
+Proof.
+move=> A B.
+rewrite /Subset.
+move=> x Hx_in_A.
+rewrite /Cup /In.
+by left.
+Qed.
+
+(* (2.2.2) *)
+Theorem subset_cup_r: forall (A B: Ensemble), B \subset A \cup B.
+Proof.
+move=> A B.
+rewrite /Subset.
+move=> x Hx_in_B.
+rewrite /Cup /In.
+by right.
+Qed.
+
+(* 2.3 *)
+Theorem subsets_cup: forall (A B C: Ensemble), A \subset C -> B \subset C -> A \cup B \subset C.
+Proof.
+move=> A B C HA_subset_C HB_subset_C.
+rewrite /Subset /Cup /In.
+move=> x.
+case.
+- by apply /HA_subset_C.
+- by apply /HB_subset_C.
+Qed.
+
+(* (2.4) *)
+Theorem cup_diag: forall A, A \cup A = A.
+Proof.
+move=> A.
+apply eq_subset.
+split.
+- by apply subsets_cup => //.
+- by apply subset_cup_l.
+Qed.
+
+Search (forall a, _ a a = _).
+
+(* (2.5) *)
+Theorem cup_comm: forall (A B: Ensemble), A \cup B = B \cup A.
+Proof.
+move=> A B.
+Search Ensemble (_ = _).
+apply eq_subset.
+split.
+- rewrite /Cup /Subset /In.
+  move=> x.
+  by apply or_comm.
+- rewrite /Cup /Subset /In.
+  move=> x.
+  by apply or_comm.
+Qed.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
