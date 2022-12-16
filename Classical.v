59e47e3fab6caa26844522c23bb16fd857e8cc64
soukouki <soukouki0@yahoo.co.jp>
2022-12-16 16:10:51 +0900

    古典論理におけるド・モルガンの法則の書き換えをiffに置き換えた

--- Classical.v
+++ Classical.v
@@ -1,5 +1,7 @@
 (* Classical関連の切り出し *)
 
+(* 古典論理における書き換えをiffにして、rewriteで使えるようにしている *)
+
 From mathcomp Require Import ssreflect.
 
 Require Export Classical_Prop.
@@ -19,3 +21,31 @@ split.
 - by apply not_and_or.
 - by apply or_not_and.
 Qed.
+
+Lemma not_imply: forall P Q: Prop, ~ (P -> Q) <-> P /\ ~Q.
+Proof.
+move=> P Q.
+split => H.
+- by apply imply_to_and.
+- inversion H => HPQ.
+  by apply /H1 /HPQ.
+Qed.
+
+Lemma not_iff: forall P Q, ~ (P <-> Q) <-> (~P /\ Q) \/ (P /\ ~Q).
+Proof.
+move=> P Q.
+rewrite {2}/iff.
+rewrite not_and_or.
+rewrite 2!not_imply.
+by rewrite or_comm {1}and_comm.
+Qed.
+
+
+
+
+
+
+
+
+
+
